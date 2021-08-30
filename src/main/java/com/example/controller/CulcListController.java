5a14ee75ad64d6415c4102f76cbbe5d064e71c0d
山口諒 <makotoyamaguchi@localhost>
2021-08-30 22:25:38 +0900

    計算がロジックの実装：コントローラーの実装、htmlでオブジェクトの入出力を実装（エラー中）

--- src/main/java/com/example/controller/CulcListController.java
+++ src/main/java/com/example/controller/CulcListController.java
@@ -1,14 +1,20 @@
 package com.example.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.CulcDate;
+import com.example.form.BaseDateForm;
 import com.example.service.CulcService;
 
 
@@ -19,8 +25,11 @@ public class CulcListController {
 	@Autowired
 	private CulcService culcService;
 	
-	@GetMapping("/list")
-	public String getCulcList(Model model) {
+	@GetMapping("")
+	public String getCulcList(@ModelAttribute BaseDateForm baseDateForm ,Model model) {
+		
+		//baseDateにデフォルトで値をセット
+		baseDateForm.setBaseDate(LocalDate.now());
 		
 		//計算一覧を取得
 		List<CulcDate> culcList = culcService.getCulcAll();
@@ -32,5 +41,17 @@ public class CulcListController {
 		return "list";
 	}
 
+	@PostMapping("")
+	public String postCulcResult(@DateTimeFormat(pattern="yyyy/MM/dd")@RequestParam("baseDate")BaseDateForm baseDateForm ,Model model) {
+		
+		//日付の加算メソッドを呼び出し
+		LocalDate resultDate = culcService.culcDate(baseDateForm.getBaseDate());
+		
+		//Modelに登録
+		model.addAttribute("resultDate", resultDate);
 
+		//list.htmlを返す
+		return "list";
+	}
+	
 }
