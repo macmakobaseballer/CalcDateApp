1a4a968e792e702c0460e2f9eea7d3b3f89ce2b5
山口諒 <makotoyamaguchi@localhost>
2021-09-05 19:49:40 +0900

    登録画面の実装：（フォームクラスを渡す処理）

--- src/main/java/com/example/controller/CalcRegisterController.java
+++ src/main/java/com/example/controller/CalcRegisterController.java
@@ -2,14 +2,17 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.CalcRegistForm;
 
 @Controller
 @RequestMapping("/calc")
 public class CalcRegisterController {
 
 	@GetMapping("/register")
-	public String getCalcRegist() {
+	public String getCalcRegist(@ModelAttribute CalcRegistForm form) {
 		return "calcregist" ;
 	}
 }
