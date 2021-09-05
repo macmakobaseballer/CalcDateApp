64d84717a61b87f545718434d43df7a743b08bb9
山口諒 <makotoyamaguchi@localhost>
2021-09-05 19:41:06 +0900

    登録画面の実装：formクラスの生成

---
+++ src/main/java/com/example/form/CalcRegistForm.java
@@ -0,0 +1,11 @@
+package com.example.form;
+
+import lombok.Data;
+
+@Data
+public class CalcRegistForm {
+	private String calcId;
+	private int calcNumYear;
+	private int calcNumMonth;
+	private int calcNumDay;
+}
