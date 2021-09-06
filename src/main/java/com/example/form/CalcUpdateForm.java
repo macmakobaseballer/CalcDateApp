0d1179c8ed74fe4dc78439ce37ce0caa27659dae
山口諒 <makotoyamaguchi@localhost>
2021-09-06 21:07:57 +0900

    更新画面の実装：formクラスの実装

---
+++ src/main/java/com/example/form/CalcUpdateForm.java
@@ -0,0 +1,11 @@
+package com.example.form;
+
+import lombok.Data;
+
+@Data
+public class CalcUpdateForm {
+	private String calcId;
+	private int calcNumYear;
+	private int calcNumMonth;
+	private int calcNumDay;
+}
