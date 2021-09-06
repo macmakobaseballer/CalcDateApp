eb6e54238c2cc66ca46a30ce3926509fb28bcfa5
山口諒 <makotoyamaguchi@localhost>
2021-09-06 21:44:00 +0900

    更新処理の実装：Update画面の実装（画面のMappingまで）

---
+++ src/main/java/com/example/controller/CalcUpdateController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.CalcUpdateForm;
+import com.example.service.CalcService;
+
+@Controller
+@RequestMapping("/calc")
+public class CalcUpdateController {
+	
+	@Autowired
+	CalcService calcService;
+	
+	@GetMapping("/update")
+	public String getCalcUpdate(@ModelAttribute CalcUpdateForm form) {
+		
+		return "calcupdate";
+		
+	}
+	
+	
+	
+	
+	
+	
+}
