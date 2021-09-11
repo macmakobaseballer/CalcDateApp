9c79aa497a9d2347df901c8abc6046bc1395b077
山口諒 <makotoyamaguchi@192.168.1.2>
2021-09-11 11:33:28 +0900

    ログイン画面の実装：controllerの修正

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class LoginController {
    
 	// ログイン画面を表示 
