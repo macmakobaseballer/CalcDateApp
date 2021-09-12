2a997502acd785e9e1e789fb375299effb1732d8
山口諒 <makotoyamaguchi@192.168.1.2>
2021-09-12 17:05:02 +0900

    バリデーションの実装：hibernate.validationに変更

--- src/main/java/com/example/form/BaseDateForm.java
+++ src/main/java/com/example/form/BaseDateForm.java
@@ -1,14 +1,18 @@
 package com.example.form;
 
+
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
+@SuppressWarnings("deprecation")
 @Data
 public class BaseDateForm {
 	
+	@NotBlank
 	@DateTimeFormat(pattern="yyyy/MM/dd")
 	private LocalDate baseDate ;
 
