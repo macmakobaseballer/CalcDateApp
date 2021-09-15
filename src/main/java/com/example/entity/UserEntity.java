cf8263c3e4ee1a4afe95c8c7fbb0567e658e391b
山口諒 <makotoyamaguchi@192.168.1.2>
2021-09-15 23:25:43 +0900

    ユーザー登録画面の実装：Entitiyクラスの実装

---
+++ src/main/java/com/example/entity/UserEntity.java
@@ -0,0 +1,13 @@
+package com.example.entity;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+	
+	private String userId;
+	private String password;
+	private String userName;
+	private String role;
+
+}
