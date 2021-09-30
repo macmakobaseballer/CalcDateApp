a79f54e1057a66955c32339b1c76bdb8f56ece97
MACBOOK_MAKO <>
2021-09-30 17:09:57 +0900

    Mapperクラスのテスト：一時完了

--- src/test/java/com/example/mapper/CalcMapperTest.java
+++ src/test/java/com/example/mapper/CalcMapperTest.java
@@ -1,8 +1,7 @@
 package com.example.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
+import static org.junit.jupiter.api.Assertions.assertNull;
 import java.util.List;
 
 import com.example.entity.DateFormula;
@@ -22,30 +21,89 @@ public class CalcMapperTest {
     private CalcMapper sut;
 
     @Test
-    public void 全件検索して結果をリストで2件取得できること () throws Exception{
+    public void 全件検索して計算式ををDBから3件取得できること () throws Exception{
         List <DateFormula> actual = sut.selectFormulas();
-
+        
         assertEquals(actual.size(),3);
+    }
+
+    @Test
+    public void 一件検索して計算式をDBから1件取得できること () throws Exception{
+        //テスト対象メソッドの呼び出し
+        DateFormula actual = sut.selectFormula(1);
+
+        //値の比較
+        assertEquals(actual.getResultId(),1);
+        assertEquals(actual.getCalcId(), "Y1M1");
+        assertEquals(actual.getCalcNumYear(),1);
+        assertEquals(actual.getCalcNumMonth(),1);
+        assertEquals(actual.getCalcNumDay(),0);
+
+    }
+        
+    @Test
+    public void 計算式をDBに1件登録できること () throws Exception{
+        //DateFormula型の変数の定義
+        DateFormula formula = createFormula("Y1D1",1,0,1);
+        //テスト対象メソッドの呼び出し
+        sut.insertFormula(formula);
+
+        //実測値の定義
+        DateFormula actual = sut.selectFormula(4);
+
+        //値の比較
+        assertEquals(actual.getResultId(),4);
+        assertEquals(actual.getCalcId(), "Y1D1");
+        assertEquals(actual.getCalcNumYear(),1);
+        assertEquals(actual.getCalcNumMonth(),0);
+        assertEquals(actual.getCalcNumDay(),1);
 
     }
 
     @Test
-    void testInsertFormula() throws Exception{
-        fail("未実装");
+    public void 計算式をDBから1件更新できること () throws Exception{
+        //DateFormula型の変数の定義;
+        DateFormula formula = sut.selectFormula(1);
+        formula.setCalcId("Y2M2");
+        formula.setCalcNumYear(2);
+        formula.setCalcNumMonth(2);
+        //テスト対象メソッドの実行
+        sut.updateFormula(formula);
+
+        //実測値の定義
+        DateFormula actual = sut.selectFormula(1);
+
+        //値の比較
+        assertEquals(actual.getResultId(),1);
+        assertEquals(actual.getCalcId(), "Y2M2");
+        assertEquals(actual.getCalcNumYear(),2);
+        assertEquals(actual.getCalcNumMonth(),2);
+        assertEquals(actual.getCalcNumDay(),0);
     }
 
-    @Test
-    void testSelectFormula() throws Exception{
-        fail("未実装");
-    }
 
     @Test
-    void testSelectFormulas() throws Exception{
-        fail("未実装");
+    public void 計算式をDBから1件削除できること() throws Exception{
+        //テスト対象メソッドの実行
+        sut.deleteFormula(2);
+
+        //実測値の定義;
+        DateFormula actual = sut.selectFormula(2);
+
+        //値の比較
+        assertNull(actual);
     }
 
-    @Test
-    void testUpdateFormula() throws Exception{
-        fail("未実装");
+    //テストデータセット用メソッド
+    private DateFormula createFormula(String calcId , int calcNumYear , int calcNumMonth , int calcNumDay ) {
+        DateFormula formula = new DateFormula();
+        formula.setCalcId(calcId);
+        formula.setCalcNumYear(calcNumYear);
+        formula.setCalcNumMonth(calcNumMonth);
+        formula.setCalcNumDay(calcNumDay);
+        return formula;
     }
+
 }
+
+
